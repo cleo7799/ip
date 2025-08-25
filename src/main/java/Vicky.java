1518e54cd2c00a8d57d4ecd9b18a4b35851aebd3
Rachel <rachelwongyx@gmail.com>
2025-08-25 17:38:49 +0800

    Added echoing

--- src/main/java/Vicky.java
+++ src/main/java/Vicky.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Vicky {
+    public static final String indent = "    ";
+
     public static void main(String[] args) {
         /*
         String logo = " ____        _        \n"
@@ -9,15 +13,29 @@ public class Vicky {
         System.out.println("Hello from\n" + logo);
 
          */
+        Scanner scanner = new Scanner(System.in);
         print_line();
-        System.out.println("Hello! I'm Vicky\nWhat can I do for you?");
+        System.out.println(indent + "Hello! I'm Vicky\n" + indent + "What can I do for you?");
         print_line();
-        System.out.println("Bye! Hope to see you again soon!");
+
+        String command = scanner.next();
+        while (!(command.equals("bye"))) {
+            if (!command.isEmpty()) {
+                System.out.println(indent + command);
+                print_line();
+            }
+            command = scanner.next();
+        }
+
+        System.out.println(indent + "Bye! Hope to see you again soon!");
         print_line();
+
+        scanner.close();
+
     }
 
     public static void print_line() {
         int length = 40;
-        System.out.println("_".repeat(length));
+        System.out.println(indent + "_".repeat(length));
     }
 }
