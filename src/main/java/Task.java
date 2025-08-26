b4764c9dfa01745b124953e99218e16152560688
Rachel <rachelwongyx@gmail.com>
2025-08-26 16:24:26 +0800

    Added Mark as Done to Vicky, Modified Task to allow mark and unmark.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,24 @@
 public class Task {
     protected String name;
+    protected boolean done;
 
     public Task(String name) {
         this.name = name;
+        this.done = false;
+    }
+
+    public void mark() {
+        this.done = true;
+    }
+
+    public void unmark() {
+        this.done = false;
     }
 
     @Override
     public String toString() {
-        return this.name;
+        char p = this.done ? 'X' : ' ';
+        return String.format("[%c] %s", p, this.name);
     }
 
 }
--- src/main/java/Vicky.java
+++ src/main/java/Vicky.java
@@ -17,14 +17,15 @@ public class Vicky {
          */
         Scanner scanner = new Scanner(System.in);
 
-
+        // Greeting
         print_line();
         System.out.println(indent + "Hello! I'm Vicky\n" + indent + "What can I do for you?");
         print_line();
 
-        String command = scanner.nextLine();
+        String command = scanner.next();
         while (!(command.equals("bye"))) {
 
+            // print list
             if (command.equals("list")) {
 
                 System.out.println(indent + "Here's your todo list:");
@@ -33,18 +34,44 @@ public class Vicky {
                 }
                 print_line();
 
-            } else if (!command.isEmpty()) {
-                System.out.println(indent + "added: " + command);
+            } else if (command.contains("unmark")) {
+                int index = scanner.nextInt() - 1;
+                if (index < counter) {
+                    todo_list[index].unmark();
+                    System.out.println(indent + "OK, I've marked this task as not done yet:");
+                    System.out.println(todo_list[index]);
+                } else {
+                    System.out.println(indent + "CHIBAI THE INDEX OUT OF BOUNDS LA");
+                }
                 print_line();
-                Task t = new Task(command);
+
+            } else if (command.contains("mark")) {
+                int index = scanner.nextInt() - 1;
+                if (index < counter) {
+                    todo_list[index].mark();
+                    System.out.println(indent + "YAY! I've marked this task as done:");
+                    System.out.println(indent + todo_list[index]);
+                } else {
+                    System.out.println(indent + "CHIBAI THE INDEX OUT OF BOUNDS LA");
+                }
+
+                print_line();
+
+            // Adding a new task
+            } else if (!command.isEmpty()) {
+                String fullCommand = command + scanner.nextLine();
+                System.out.println(indent + "added: " + fullCommand);
+                print_line();
+                Task t = new Task(fullCommand);
                 Vicky.todo_list[counter] = t;
                 Vicky.counter++;
 
             }
 
-            command = scanner.nextLine();
+            command = scanner.next();
         }
 
+        // Goodbye message
         System.out.println(indent + "Bye! Hope to see you again soon!");
         print_line();
 
